2c5aa391dcce86228fd620cb36512f9f752e7e63
Tracy <cary0522@gmail.com>
2026-05-26 13:32:32 +0800

    修正變數命名，統一使用小寫格式以符合慣例

--- app/pages/dp_index.vue
+++ app/pages/dp_index.vue
@@ -5,17 +5,17 @@ import { ref } from 'vue';
 // 有網站資料之後，才能取得這個使用者所有後台頁籤
 // 頁籤:增刪修查api
 
-const SiteName = ref('')
-const SiteMenuData = ref({
-    Name: '',
-    Slug: '',
-    ViewType: '',
+const siteName = ref('')
+const siteMenuData = ref({
+    name: '',
+    slug: '',
+    viewType: '',
 })
-const SiteMenuList = ref([
+const siteMenuList = ref([
     {
-        Name: '',
-        Slug: '',
-        ViewType: '',
+        name: '',
+        slug: '',
+        viewType: '',
     },
 ])
 
@@ -31,20 +31,20 @@ const SiteMenuList = ref([
                     <h3>
                         網站管理
                     </h3>
-                    <input type="text" v-model="SiteName" placeholder="網站名稱" class="border p-1" />
+                    <input type="text" v-model="siteName" placeholder="網站名稱" class="border p-1" />
                 </div>
                 <div>
                     <h3>
                         頁籤管理
                     </h3>
-                    <div v-for="menu in SiteMenuList" :key="menu.Slug" class="flex items-center">
-                        <p>{{ menu.Name }}</p>
-                        <p>{{ menu.Slug }}</p>
-                        <p>{{ menu.ViewType }}</p>
+                    <div v-for="menu in siteMenuList" :key="menu.slug" class="flex items-center">
+                        <p>{{ menu.name }}</p>
+                        <p>{{ menu.slug }}</p>
+                        <p>{{ menu.viewType }}</p>
                     </div>
-                    <input type="text" v-model="SiteMenuData.Name" placeholder="頁籤名稱" class="border p-1" />
-                    <input type="text" v-model="SiteMenuData.Slug" placeholder="頁籤slug" class="border p-1" />
-                    <input type="text" v-model="SiteMenuData.ViewType" placeholder="頁籤view type" class="border p-1" />
+                    <input type="text" v-model="siteMenuData.name" placeholder="頁籤名稱" class="border p-1" />
+                    <input type="text" v-model="siteMenuData.slug" placeholder="頁籤slug" class="border p-1" />
+                    <input type="text" v-model="siteMenuData.viewType" placeholder="頁籤view type" class="border p-1" />
                 </div>
             </div>
         </div>
--- app/pages/user/register.vue
+++ app/pages/user/register.vue
@@ -2,20 +2,20 @@
 
 // 註冊畫面
 
-const UserData = {
+const userData = {
     account: '',
     email: '',
     password: '',
 }
 
 function Register() {
-    if (!UserData.account || !UserData.email || !UserData.password) {
+    if (!userData.account || !userData.email || !userData.password) {
         alert('請輸入帳號、Email 以及密碼')
         return
     }
     $fetch('/api/auth/register', {
         method: 'POST',
-        body: JSON.stringify(UserData),
+        body: JSON.stringify(userData),
     }).then((res) => {
         // 註冊成功，處理回傳的使用者資料（例如存到 Vuex 或 Pinia）
         console.log('註冊成功', res)
@@ -31,9 +31,9 @@ function Register() {
     <LayoutUser>
         <div class="flex h-screen items-center justify-center">
             <h1 class="text-4xl font-bold">使用者註冊畫面</h1>
-            <input type="text" placeholder="帳號" v-model="UserData.account">
-            <input type="text" placeholder="Email" v-model="UserData.email">
-            <input type="password" placeholder="密碼" v-model="UserData.password">
+            <input type="text" placeholder="帳號" v-model="userData.account">
+            <input type="text" placeholder="Email" v-model="userData.email">
+            <input type="password" placeholder="密碼" v-model="userData.password">
             <button @click="Register">註冊</button>
         </div>
     </LayoutUser>
