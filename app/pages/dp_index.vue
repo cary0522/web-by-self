3077cce059c39c0a71bf3e6c26ce7c70e0cfd208
Tracy <cary0522@gmail.com>
2026-05-26 14:31:33 +0800

    新增登出功能，使用 sweetalert2 顯示登出成功提示並導向登入頁面

--- app/pages/dp_index.vue
+++ app/pages/dp_index.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import Swal from 'sweetalert2';
 import { ref } from 'vue';
+
+const adminStore = useAdminStore()
+const router = useRouter()
 // 取得這個網站的資料
 // 網站資料:增刪修查api
 // 有網站資料之後，才能取得這個使用者所有後台頁籤
@@ -19,34 +23,52 @@ const siteMenuList = ref([
     },
 ])
 
+function LogOut() {
+    adminStore.logOut().then(() => {
+        Swal.fire({
+            icon: 'success',
+            title: '登出成功',
+            timer: 1500,
+            showConfirmButton: false,
+        }).then(() => {
+            router.push('/user/login')
+        })
+    })
+}
+
 </script>
 
 <template>
-    <div class="h-screen">
-        <h1 class="text-4xl font-bold w-full">管理者後台</h1>
-        <div class="w-full px-2 flex">
-            <div class="w-1/6 border min-h-screen">選單</div>
-            <div class="grow border min-h-screen">
-                <div>
-                    <h3>
-                        網站管理
-                    </h3>
-                    <input type="text" v-model="siteName" placeholder="網站名稱" class="border p-1" />
+    <LayoutAdmin>
+        <div class="h-screen">
+            <h1 class="text-4xl font-bold w-full">管理者後台</h1>
+            <div class="w-full px-2 flex">
+                <div class="w-1/6 border min-h-screen">選單
+                    <button @click="LogOut">登入</button>
                 </div>
-                <div>
-                    <h3>
-                        頁籤管理
-                    </h3>
-                    <div v-for="menu in siteMenuList" :key="menu.slug" class="flex items-center">
-                        <p>{{ menu.name }}</p>
-                        <p>{{ menu.slug }}</p>
-                        <p>{{ menu.viewType }}</p>
+                <div class="grow border min-h-screen">
+                    <div>
+                        <h3>
+                            網站管理
+                        </h3>
+                        <input type="text" v-model="siteName" placeholder="網站名稱" class="border p-1" />
                     </div>
-                    <input type="text" v-model="siteMenuData.name" placeholder="頁籤名稱" class="border p-1" />
-                    <input type="text" v-model="siteMenuData.slug" placeholder="頁籤slug" class="border p-1" />
-                    <input type="text" v-model="siteMenuData.viewType" placeholder="頁籤view type" class="border p-1" />
+                    <div>
+                        <h3>
+                            頁籤管理
+                        </h3>
+                        <div v-for="menu in siteMenuList" :key="menu.slug" class="flex items-center">
+                            <p>{{ menu.name }}</p>
+                            <p>{{ menu.slug }}</p>
+                            <p>{{ menu.viewType }}</p>
+                        </div>
+                        <input type="text" v-model="siteMenuData.name" placeholder="頁籤名稱" class="border p-1" />
+                        <input type="text" v-model="siteMenuData.slug" placeholder="頁籤slug" class="border p-1" />
+                        <input type="text" v-model="siteMenuData.viewType" placeholder="頁籤view type"
+                            class="border p-1" />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </LayoutAdmin>
 </template>
