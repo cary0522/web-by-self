bc6af45b0c4640fecd3eafe3ffa907702f6770e6
Tracy <cary0522@gmail.com>
2026-05-26 17:40:38 +0800

    新增 Button 共用元件，支援標題和點擊功能的屬性定義

---
+++ app/components/ui/Button.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+    title: string,
+    clickFunction: () => void
+}>()
+
+</script>
+<template>
+    <button @click="props.clickFunction" class="bg-blue-500 text-white px-4 py-2 my-2 mx-2 rounded hover:bg-blue-600">{{
+        props.title }}</button>
+</template>
