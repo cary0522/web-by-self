be1e025d7adbedbdb853bf010854cd3f014b11bb
Tracy <cary0522@gmail.com>
2026-06-22 21:56:15 +0800

    引用 requiresPageForViewType 並修正 API 邏輯，加入頁籤 ID 檢查及頁面建立邏輯，優化更新流程

--- server/api/siteMenu/create.post.ts
+++ server/api/siteMenu/create.post.ts
@@ -1,3 +1,5 @@
+import { requiresPageForViewType } from '../../utils/viewType'
+
 // /api/siteMenu/:id 取得網站 menu 設定
 export default defineEventHandler(async (event) => {
     const authUser = requireAuth(event)
@@ -22,16 +24,44 @@ export default defineEventHandler(async (event) => {
             statusMessage: '網站 viewType 為必填',
         })
     }
+    if (!body.siteId) {
+        throw createError({
+            statusCode: 400,
+            statusMessage: '網站 ID 為必填',
+        })
+    }
 
-    const res = await prisma.siteMenu.create({
-        data: {
-            name: body.name,
-            slug: body.slug,
-            viewTypeId: body.viewTypeId,
-            siteId: body.siteId,
-            parentId: body.parentId,
-            createdById: authUser.id,
-        },
+    const needsPage = await requiresPageForViewType(body.viewTypeId)
+
+    const res = await prisma.$transaction(async (tx) => {
+        let pageId: number | null = null
+
+        if (needsPage) {
+            const page = await tx.page.create({
+                data: {
+                    title: body.name,
+                    content: body.content ?? null,
+                    status: 1,
+                    siteId: body.siteId,
+                    createdBy: authUser.id,
+                    updatedBy: authUser.id,
+                },
+            })
+
+            pageId = page.id
+        }
+
+        return tx.siteMenu.create({
+            data: {
+                name: body.name,
+                slug: body.slug,
+                viewTypeId: body.viewTypeId,
+                siteId: body.siteId,
+                parentId: body.parentId,
+                pageId,
+                createdById: authUser.id,
+            },
+        })
     })
 
     return {
@@ -39,6 +69,7 @@ export default defineEventHandler(async (event) => {
         name: res.name,
         slug: res.slug,
         viewTypeId: res.viewTypeId,
-        parentId:res.parentId
+        parentId: res.parentId,
+        pageId: res.pageId,
     }
 })
--- server/api/siteMenu/update.put.ts
+++ server/api/siteMenu/update.put.ts
@@ -1,12 +1,14 @@
+import { requiresPageForViewType } from '../../utils/viewType'
+
 // /api/siteMenu/update 更新網站設定
 export default defineEventHandler(async (event) => {
     requireAuth(event)
     const body = await readBody(event)
 
-    if (!body.siteId) {
+    if (!body.menuId) {
         throw createError({
             statusCode: 400,
-            statusMessage: '網站 ID 為必填',
+            statusMessage: '頁籤 ID 為必填',
         })
     }
 
@@ -17,16 +19,60 @@ export default defineEventHandler(async (event) => {
         })
     }
 
-    const res = await prisma.siteMenu.update({
-        where: {
-            id: body.siteId,
-        },
-        data: {
-            name: body.name,
-            slug: body.slug,
-            viewTypeId: body.viewTypeId,
-            parentId: body.parentId,
-        },
+    const needsPage = body.viewTypeId ? await requiresPageForViewType(body.viewTypeId) : false
+
+    const res = await prisma.$transaction(async (tx) => {
+        const currentMenu = await tx.siteMenu.findUnique({
+            where: {
+                id: body.menuId,
+            },
+        })
+
+        if (!currentMenu) {
+            throw createError({
+                statusCode: 404,
+                statusMessage: '頁籤不存在',
+            })
+        }
+
+        let pageId = currentMenu.pageId
+
+        if (needsPage && !pageId) {
+            const page = await tx.page.create({
+                data: {
+                    title: body.name,
+                    content: body.content ?? null,
+                    status: 1,
+                    siteId: currentMenu.siteId,
+                    createdBy: currentMenu.createdById,
+                    updatedBy: currentMenu.createdById,
+                },
+            })
+            pageId = page.id
+        }
+
+        if (needsPage && pageId) {
+            await tx.page.update({
+                where: { id: pageId },
+                data: {
+                    title: body.name,
+                    content: body.content ?? undefined,
+                },
+            })
+        }
+
+        return tx.siteMenu.update({
+            where: {
+                id: body.menuId,
+            },
+            data: {
+                name: body.name,
+                slug: body.slug,
+                viewTypeId: body.viewTypeId,
+                parentId: body.parentId,
+                pageId,
+            },
+        })
     })
 
     return {
@@ -35,5 +81,6 @@ export default defineEventHandler(async (event) => {
         slug: res.slug,
         viewTypeId: res.viewTypeId,
         parentId: res.parentId,
+        pageId: res.pageId,
     }
 })
