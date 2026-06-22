6f6857b0f4fb719e4b5c7842efd7f3fbfccc6646
Tracy <cary0522@gmail.com>
2026-06-22 17:33:17 +0800

    修正 get-option API 回傳類型，引用共用 ViewType 介面定義，明確指定類別

--- server/api/viewtype/get-option.get.ts
+++ server/api/viewtype/get-option.get.ts
@@ -1,5 +1,7 @@
+import type { ViewType } from '~/types/view-type'
+
 export default defineEventHandler(async () => {
     const res = await prisma.viewType.findMany()
 
-    return res
+    return res as ViewType[]
 })
