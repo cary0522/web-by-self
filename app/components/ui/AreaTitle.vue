c23dd166d3609ca8bd50e93a4d561764c096d3dd
Tracy <cary0522@gmail.com>
2026-06-22 17:34:00 +0800

    新增共用元件：標題元件、頁籤元件

---
+++ app/components/ui/AreaTitle.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const props = defineProps<{
+    title: string,
+}>()
+
+</script>
+<template>
+    <p class="w-full font-bold text-lg my-4 mx-4 hover:underline underline-offset-8">{{
+        props.title }}</p>
+</template>
---
+++ app/components/ui/SiteMenu.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { SiteMenu } from '~/types/site-menu'
+
+const props = defineProps<{
+    data: SiteMenu
+}>()
+
+import useOptions from '~/composables/useOptions'
+const { ViewTypeList, GetViewTypeList } = useOptions()
+
+onMounted(() => {
+    GetViewTypeList()
+})
+</script>
+<template>
+    <div class="flex items-center">
+        <p class="mx-2">頁籤名稱：
+            <input type="text" v-model="data.name" />
+        </p>
+        <p class="mx-2">Slug：
+            <input type="text" v-model="data.slug" />
+        </p>
+        <label for="viewType">頁籤類別：</label>
+        <select name="viewType" id="viewType" v-model="data.viewTypeId">
+            <option v-for="option in ViewTypeList" :key="option.id" :value="option.id">
+                {{ option.name }}
+            </option>
+        </select>
+    </div>
+</template>
